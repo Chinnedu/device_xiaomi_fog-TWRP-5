92d7940f6e80d69375f21a91ddf2e253dfca4190
infinixs5lite <kelvinchinedu97@gmail.com>
2023-07-26 07:27:08 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -74,7 +74,8 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_FLASHLIGHT_ENABLE=1
         export OF_FL_PATH1="/sys/class/leds/led_torch_2"
 	export FOX_VARIANT=MIUI
-	
+	export OF_LEGACY_PROCESS_FSTAB=1
+ 
         # kernel source to be use when building
         export OF_FORCE_PREBUILT_KERNEL=1
 	
